9d8dffa3d37f91c9615befbee40a577a8fdfaaa7
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-11-19 12:09:14 -0300

    install mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
+// Nest
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+// App
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// Modules
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/blog'),
+    ProductsModule,
+    UsersModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
