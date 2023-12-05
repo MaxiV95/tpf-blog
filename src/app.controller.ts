efd0647fdd240ab6a383f314eee3828077993313
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-12-05 10:37:35 -0300

    httpExceptionFilter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './httpExceptionFilter';
+
+class EjemploHttpException extends HttpException {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
 
 @Controller()
+@UseFilters(new HttpExceptionFilter())
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    throw new EjemploHttpException('mi app fallo :(');
     return this.appService.getHello();
   }
 }
---
+++ src/httpExceptionFilter.ts
@@ -0,0 +1,19 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const status = exception.getStatus();
+    const message = exception.getResponse();
+
+    response.status(status).json({ message: message });
+  }
+}
