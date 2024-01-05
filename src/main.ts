b32e387ace7d3b01357cbbb461a0d8bad410bd39
MaxiV95 <maximilianovanmegroot@gmail.com>
2024-01-05 16:27:33 -0300

    main config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+//main.ts
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const PORT = 3001;
+const PORT = process.env.PORT; //|| 3001;
 
 async function bootstrap() {
   try {
@@ -11,10 +12,10 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
 
     const config = new DocumentBuilder()
-      .setTitle('Ejemplo API')
-      .setDescription('La descripción de la API')
+      .setTitle('TPF Blog cf')
+      .setDescription('Documentación de la API')
       .setVersion('0.0.1')
-      .addTag('etiqueta')
+      .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, document);
