fbafc2f8420c1ad34a298fc8620a11db6ca298a6
MaxiV95 <maximilianovanmegroot@gmail.com>
2024-01-09 17:11:41 -0300

    auth

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -11,8 +11,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string) {
+    if (email.length===0 || password.length===0) throw new UnauthorizedException('Email and password is required');
     const user = await this.authService.validateUser(email, password);
-    if (!user) throw new UnauthorizedException('Email and password is required');
+    if (!user) throw new UnauthorizedException('Incorrect email or password');
     const token = this.authService.login(user);
     return token;
   }
