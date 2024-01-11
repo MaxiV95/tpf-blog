2e9fde55e1bd5f6f845f0e505055fd4f01b57e50
MaxiV95 <maximilianovanmegroot@gmail.com>
2024-01-11 00:03:07 -0300

    errorExceptionFilters

--- src/errorExceptionFilters.ts
+++ src/errorExceptionFilters.ts
@@ -1,21 +1,25 @@
 //errorExceptionFilters.ts
 import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { Response } from 'express';
+import { MongoServerError } from 'mongodb';
 
-export class CustomError extends Error {
-  constructor(
-    message: string,
-    public status: number,
-    name: string = 'CustomError',
-  ) {
-    super(message);
-    this.name = name;
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const handler =
+      ERROR_HANDLERS[exception.name] || ERROR_HANDLERS.defaultError;
+    handler(response, exception);
   }
 }
 
 const ERROR_HANDLERS = {
   BadRequestException: (res: any, err: Error) => {
     res.status(400).json({ name: err.name, message: err.message }).end();
+  },
+  MongoServerError: (res: any, err: MongoServerError) => {
+    res.status(409).json({ name: err.name, message: err.message, code: err.code }).end();
   },
   NotFoundException: (res: any, err: Error) => {
     res.status(404).json({ name: err.name, message: err.message }).end();
@@ -34,14 +38,3 @@ const ERROR_HANDLERS = {
     res.status(status).json({ name, message }).end();
   },
 };
-
-@Catch(Error)
-export class ErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
-    const context = host.switchToHttp();
-    const response = context.getResponse<Response>();
-    const handler =
-      ERROR_HANDLERS[exception.name] || ERROR_HANDLERS.defaultError;
-    handler(response, exception);
-  }
-}
