a9ccaaca81bdbbb209816b9550aa58439015d458
MaxiV95 <maximilianovanmegroot@gmail.com>
2024-01-17 14:20:26 -0300

    admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -8,7 +8,7 @@ import {
   UseGuards,
   HttpCode,
 } from '@nestjs/common';
-import { UserDB } from 'src/users/user.dto';
+import { UserAuthDto, UserDB } from 'src/users/user.dto';
 import {
   ApiBearerAuth,
   ApiOperation,
@@ -19,11 +19,12 @@ import { AdminService } from './admin.service';
 import { ErrorFilter } from 'src/errorExceptionFilters';
 import { JwtAuthGuard } from 'src/auth/strategy/jwt-auth.guard';
 import { adminGuard } from 'src/auth/guard/admin.guard';
+import { User } from 'src/auth/custom.decorator.ts';
+import { ReducedPostDB } from 'src/posts/post.dto';
 
-@ApiTags('Admin')
+@ApiTags('Administration')
 @Controller('admin')
 @UseFilters(ErrorFilter)
-@UseGuards(JwtAuthGuard, adminGuard)
 @ApiBearerAuth()
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -35,6 +36,7 @@ export class AdminController {
     description: 'Retorna todos los usuarios.',
     type: [UserDB],
   })
+  @UseGuards(JwtAuthGuard, adminGuard)
   @HttpCode(200)
   async getAllUsers(): Promise<UserDB[]> {
     return await this.adminService.getAllUsers();
@@ -56,21 +58,26 @@ export class AdminController {
       },
     },
   })
+  @UseGuards(JwtAuthGuard, adminGuard)
   @HttpCode(200)
   async deleteUser(@Param('id') id: string) {
     return await this.adminService.deleteUser(id);
   }
 
-  // @Get('posts')
-  // @ApiOperation({
-  //   summary: 'ADMIN Obtener todos los posts con opciones de moderación',
-  // })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'Retorna todos los posts con opciones de moderación.',
-  //   type: [PostDB],
-  // })
-  // getAllPosts() {
-  //   return this.adminService.getAllPosts();
-  // }
+  @Get('posts')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary:
+      'Obtener todos los posts con opciones de moderación (borrar o editar)',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna todos los posts con opciones de moderación.',
+    type: [ReducedPostDB],
+  })
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(200)
+  async getAllPosts(@User() user: UserAuthDto): Promise<ReducedPostDB[]> {
+    return await this.adminService.getAllPosts(user);
+  }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -4,7 +4,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Post } from 'src/posts/post.schema';
 import { User } from 'src/users/user.schema';
-import { UserDB } from 'src/users/user.dto';
+import { UserAuthDto, UserDB } from 'src/users/user.dto';
+import { ReducedPostDB } from 'src/posts/post.dto';
+import { toJSON } from 'src/posts/postToJSON';
 
 @Injectable()
 export class AdminService {
@@ -22,19 +24,12 @@ export class AdminService {
     return users.map((user) => user.toJSON());
   }
 
-  // async getAllPosts() {
-  //   const posts = this.postModel.find().lean();
-  //   const moderatedPosts = this.applyModerationOptions(posts);
-  //   return moderatedPosts;
-  // }
-
-  // private applyModerationOptions(posts: PostDB[]): ModeratedPostDTO[] {
-  //   return posts.map((post) => ({
-  //     ...post,
-  //     moderationOptions: {
-  //       delete: true,
-  //       edit: true,
-  //     },
-  //   }));
-  // }
+  async getAllPosts(user: UserAuthDto): Promise<ReducedPostDB[]> {
+    const query = user.admin ? {} : { idAuthor: user.id };
+    const posts = await this.postModel
+      .find(query)
+      .select('-idAuthor -categories')
+      .exec();
+    return posts.map((post) => toJSON(post));
+  }
 }
